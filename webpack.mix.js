ff5b6d5d7accd4a8d0cf72b1cb53544f890a76b3
chrisjk123 <chris_j_kelker@hotmail.com>
2020-02-05 16:31:03 +0000

    Created new assets compiling path for global files

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,15 +19,18 @@ mix.webpackConfig({
  |
  */
 
-mix.scripts([
-    'node_modules/js-cookie/src/js.cookie.js',
-    'resources/assets/js/gdpr-cookie-notice/templates.js',
-    'resources/assets/js/gdpr-cookie-notice/script.js',
-    'resources/assets/js/gdpr-cookie-notice/en.js'
-], 'public/js/gdpr-cookie-notice.js');
+// mix.scripts([
+//     'node_modules/js-cookie/src/js.cookie.js',
+//     'resources/assets/js/gdpr-cookie-notice/templates.js',
+//     'resources/assets/js/gdpr-cookie-notice/script.js',
+//     'resources/assets/js/gdpr-cookie-notice/en.js'
+// ], 'public/js/gdpr-cookie-notice.js');
+//
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css')
+//    .browserSync({
+//        proxy: 'fixo.meter:8888'
+//     });
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .browserSync({
-       proxy: 'fixo.meter:8888'
-    });
+mix.js('resources/global/js/app.js', 'public/global/js')
+   .sass('resources/global/css/app.scss', 'public/global/css');
