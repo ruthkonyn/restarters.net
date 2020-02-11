78ce36b60e41533dd3eed66ec8658e20b653752f
Russ Mitchell <russ@Russs-MacBook-Pro.local>
2020-02-11 16:52:21 +0000

    WIP: work on the groups section turning the content into accordion - still a work in progress

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -20,6 +20,7 @@
       </div>
       @endif
 
+
       <div class="row">
         <div class="col">
           <div class="d-flex justify-content-between align-content-center">
@@ -61,23 +62,98 @@
         <form action="/group/" method="get" id="device-search">
           <input type="hidden" name="sort_direction" value="{{$sort_direction}}" class="sr-only">
           <input type="radio" name="sort_column" value="upcoming_event" @if( $sort_column == 'upcoming_event' ) checked @endif id="label-upcoming_event" class="sr-only">
-
-          @if( !is_null($your_groups) )
+<div class="collapse offset-md-box-shadow d-md-block show" id="collapseSearchFilters">
+  <div class="nav-md-tabs">
+    <ul id="tabs" class="nav nav-tabs nav-tabs-block nav-md-tabs" role="tablist">
+      <li class="nav-item">
+        <a id="tab-A" href="#pane-A" class="nav-link white active" data-toggle="tab" role="tab">
+          @lang('groups.groups_title1')
+        </a>
+      </li>
+      <li class="nav-item">
+        <a id="tab-B" href="#pane-B" class="nav-link white " data-toggle="tab" role="tab">
+          @lang('groups.groups_title2')
+        </a>
+      </li>
+      <li class="nav-item">
+        <a id="tab-C" href="#pane-C" class="nav-link white" data-toggle="tab" role="tab">
+          @lang('groups.groups_title3')
+        </a>
+      </li>
+    </ul>
+<div id="content" class="tab-content" role="tablist">
+  <div id="pane-A" class="tab-pane fade show active" role="tabpanel" aria-labelledby="tab-A">
+    <div class="accordion-tab-header white collapse-plus-and-minus" role="tab" id="heading-A">
+      <button class="btn" data-toggle="collapse" data-target="#collapse-A" aria-expanded="true" aria-controls="collapse-A">
+        <h5>
+          @lang('groups.groups_title1')
+        </h5>
+      </button>
+    </div>
+    <div id="collapse-A" class="collapse collapse-wrapper show" data-parent="#content" role="tabpanel" aria-labelledby="heading-A">
+      <div class="collapse-content white">
+        <div class="row">
+          <div class="col-12 col-md-12 form-group">
+            @if( !is_null($your_groups) )
+                 <div class="row">
+                   <div class="col">
+                     @include('group.sections.user-groups')
+                   </div>
+                 </div>
+               @endif
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+  <div id="pane-B" class="tab-pane fade" role="tabpanel" aria-labelledby="tab-B">
+    <div class="accordion-tab-header white collapse-plus-and-minus" role="tab" id="heading-B">
+      <button class="btn collapsed" data-toggle="collapse" data-target="#collapse-B" aria-expanded="true" aria-controls="collapse-B">
+        <h5>
+          @lang('groups.groups_title3')
+        </h5>
+      </button>
+    </div>
+        <div id="collapse-B" class="collapse collapse-wrapper show" data-parent="#content" role="tabpanel" aria-labelledby="heading-B">
+          <div class="collapse-content white">
             <div class="row">
-              <div class="col">
-                @include('group.sections.user-groups')
+              <div class="col-12 col-md-12 form-group">
+                  <div class="row">
+                    <div class="col">
+                      @include('group.sections.groups-nearby')
+                    </div>
+                  </div>
               </div>
             </div>
-          @endif
-
-          @if( is_null($groups) )
-            <div class="row">
-              <div class="col">
-                @include('group.sections.groups-nearby')
-              </div>
+        </div>
+      </div>
+</div>
+<div id="pane-C" class="tab-pane fade" role="tabpanel" aria-labelledby="tab-C">
+  <div class="accordion-tab-header white collapse-plus-and-minus" role="tab" id="heading-C">
+    <button class="btn collapsed" data-toggle="collapse" data-target="#collapse-C" aria-expanded="true" aria-controls="collapse-C">
+      <h5>
+        @lang('groups.groups_title2')
+      </h5>
+    </button>
+  </div>
+      <div id="collapse-C" class="collapse collapse-wrapper show" data-parent="#content" role="tabpanel" aria-labelledby="heading-C">
+        <div class="collapse-content white">
+          <div class="row">
+            <div class="col-12 col-md-12 form-group">
+                <div class="row">
+                  <div class="col">
+                    @include('group.sections.all-groups')
+                  </div>
+                </div>
             </div>
-          @endif
-        </form>
+          </div>
+      </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</form>
         @php( $user_preferences = session('column_preferences') )
       @endif
 
