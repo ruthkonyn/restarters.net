d0cec091509c64112b40197c95cc98c545a5577e
Steven Hardy <steven@wecreatedigital.co.uk>
2020-03-11 12:22:34 +0000

    Talk JS Changes

--- resources/talk/js/app.js
+++ resources/talk/js/app.js
@@ -33,27 +33,29 @@ function addActive(tab) {
   tab.classList.add('active');
 }
 
-$('.toggle-dropdown-menu').click(function(e) {
-  e.preventDefault();
-  // If item is already active then close all.
-  if ( $(this).hasClass('dropdown-active')) {
-    $('.toggle-dropdown-menu').each(function() {
+(function() {
+  $('.toggle-dropdown-menu').click(function(e) {
+    e.preventDefault();
+    // If item is already active then close all.
+    if ( $(this).hasClass('dropdown-active')) {
+      $('.toggle-dropdown-menu').each(function() {
+        $(this).removeClass('dropdown-active');
+        $(this).parents().children('.dropdown-menu-items').hide();
+      });
+
+      return false;
+    }
+
+    // Close all existing items except current.
+    $('.toggle-dropdown-menu').not(this).each(function() {
       $(this).removeClass('dropdown-active');
       $(this).parents().children('.dropdown-menu-items').hide();
     });
 
-    return false;
-  }
-
-  // Close all existing items except current.
-  $('.toggle-dropdown-menu').not(this).each(function() {
-    $(this).removeClass('dropdown-active');
-    $(this).parents().children('.dropdown-menu-items').hide();
+    // Show items.
+    $(this).toggleClass('dropdown-active');
+    $(this).parents().children('.dropdown-menu-items').toggle();
   });
-
-  // Show items.
-  $(this).toggleClass('dropdown-active');
-  $(this).parents().children('.dropdown-menu-items').toggle();
 });
 
 setTimeout(function() {
