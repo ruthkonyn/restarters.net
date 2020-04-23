0913d910d98c4f19c8075c2d34c54db8c724c242
Dean Claydon <dean@wecreatedigital.co.uk>
2020-04-23 10:04:42 +0100

    Alt approach for inbox drop down

--- resources/talk/js/app.js
+++ resources/talk/js/app.js
@@ -129,9 +129,9 @@ function clearDropdowns() {
 
 // Used on the inbox drop down in Discourse
 function navigateUrl(item) {
-  if(item.value) {
-    location.href = document.location.origin + item.value;
-  }
+  $('#messages-dropdown').on('change', function(){
+    location.href = document.location.origin + $(this).val();
+  });
 }
 
 function setInboxDropdownState() {
